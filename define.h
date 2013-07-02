cd4daeafd2b91575dc76bb942a9d1ffefeab558d
Quirin <Quirin.Koerner@tum.de>
2013-07-02 16:10:18 +0200

    ALLES GEHT

--- define.h
+++ define.h
@@ -9,12 +9,9 @@
 #define SUPERLANGSAM 10
 #define MAX 99
 
-<<<<<<< HEAD
 #define HOEHENSCHUBMULTIPLIKATOR 5
 
 #define SIMULATIONMAX 250
-=======
-#define SIMULATIONMAX 800
->>>>>>> 7575caba334d30e17ddc653994b78cf5932ff472
+
 
 #endif // DEFINE_H
